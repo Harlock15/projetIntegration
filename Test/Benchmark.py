7ecc6d6eae8ae6e74c0fd1e5a0695af867fa0ec5
Barberey <riedel.romain@gmail.com>
2024-01-18 12:45:44 +0100

    Result Benchmark dans un fichier

--- Test/Benchmark.py
+++ Test/Benchmark.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from Algo.Negamax import Negamax
@@ -20,44 +21,47 @@ class Benchmark:
 
 
     def testNegamaxFich(self, fich):
-        self.nbrTotTest = 0
-        self.tmpTot = 0
-        self.nodeParcTot = 0
-        self.nbrTestOk = 0
-        with open(f"./TestFile/{fich}", 'r') as fichier:
-            lignes = fichier.readlines()
-            self.nbrTotTest = len(lignes)
-            for x in range(self.nbrTotTest):
-                ligne = lignes[x].strip().split(' ')
+        with open(f"./result", "w") as res:
+            res.write(f"[{datetime.datetime.now()}]\n")
+            self.nbrTotTest = 0
+            self.tmpTot = 0
+            self.nodeParcTot = 0
+            self.nbrTestOk = 0
+            with open(f"./TestFile/{fich}", 'r') as fichier:
+                lignes = fichier.readlines()
+                self.nbrTotTest = len(lignes)
+                for x in range(self.nbrTotTest):
+                    ligne = lignes[x].strip().split(' ')
 
-                scoreAtt = ligne[1]
-                coup_joue = ligne[0]
+                    scoreAtt = ligne[1]
+                    coup_joue = ligne[0]
 
-                pos = Position(coup_joue)
+                    pos = Position(coup_joue)
 
-                s = Negamax()
-                start = time.time()
-                weak = False
-                scoreObt = s.solve(pos)
-                self.tmpTot += time.time() - start
+                    s = Negamax()
+                    start = time.time()
+                    weak = False
+                    scoreObt = s.solve(pos)
+                    self.tmpTot += time.time() - start
 
-                self.nodeParcTot += s.noeuds_parcourus
-                if (str(scoreObt) != scoreAtt):
-                    print(f"Erreur Ligne {x + 1} | ScoreObt: {scoreObt} ScoreAtt: {scoreAtt}")
-                else:
-                    if((x+1)%100 == 0):
-                        print(f"Test {x + 1}: OK")
-                    self.nbrTestOk += 1
+                    self.nodeParcTot += s.noeuds_parcourus
+                    if (str(scoreObt) != scoreAtt):
+                        res.write(f"[{datetime.datetime.now()}] [{fich}] Erreur Ligne {x + 1} | ScoreObt: {scoreObt} ScoreAtt: {scoreAtt}\n")
+                    else:
+                        if((x+1)%100 == 0):
+                            print(f"Test {x + 1}: OK")
+                        self.nbrTestOk += 1
 
-            self.nodeParcMoy = self.nodeParcTot / self.nbrTotTest
-            self.moyTmp = self.tmpTot / self.nbrTotTest
+                self.nodeParcMoy = self.nodeParcTot / self.nbrTotTest
+                self.moyTmp = self.tmpTot / self.nbrTotTest
 
 
-            print(f"----------\nFichier {fich}\nTest reussi: {self.nbrTestOk}/{self.nbrTotTest}\nMoy Noeud Parcourus: {self.nodeParcMoy}\nTmp Moy: {self.moyTmp}\n----------")
+                print(f"----------\nFichier {fich}\nTest reussi: {self.nbrTestOk}/{self.nbrTotTest}\nMoy Noeud Parcourus: {self.nodeParcMoy}\nTmp Moy: {self.moyTmp}\n----------")
+                res.write(f"----------\n[{datetime.datetime.now()}]\nFichier {fich}\nTest reussi: {self.nbrTestOk}/{self.nbrTotTest}\nMoy Noeud Parcourus: {self.nodeParcMoy}\nTmp Moy: {self.moyTmp}\n----------")
 
 
 
 
 if __name__ == "__main__":
     bench = Benchmark()
-    bench.testNegamax()
+    bench.testNegamaxFich("Test_L3_R1")
