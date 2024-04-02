23fce28b4db8cfbd91c9c5807bccfd5ace958809
Harlock15 <92543161+Harlock15@users.noreply.github.com>
2024-04-02 16:38:12 +0200

    Update OldPosition.py

--- Algo/OldPosition.py
+++ Algo/OldPosition.py
@@ -1,49 +1,65 @@
 class Position:
-    def __init__(self, coup_joue:str):
+    def __init__(self, coup_joue: str):
+        # Initialisation des dimensions du plateau de jeu
         self.WIDTH = 7
         self.HEIGHT = 6
+        # Stockage de la séquence des coups joués
         self.coup_joue = coup_joue
+        # Initialisation du plateau de jeu, de la hauteur des colonnes et du nombre de coups joués
         self.board, self.height, self.moves = self.readString()
 
+    # Méthode pour vérifier si un coup peut être joué dans une colonne
     def canPlay(self, colonne):
         return self.height[colonne] < self.HEIGHT
 
+    # Méthode pour jouer un coup dans une colonne
     def play(self, colonne):
-        self.board[colonne][self.height[colonne]] = 1 + self.moves%2
+        # Mise à jour du plateau, de la hauteur de la colonne et du nombre de coups joués
+        self.board[colonne][self.height[colonne]] = 1 + self.moves % 2
         self.height[colonne] += 1
         self.moves += 1
-        self.coup_joue += str(colonne+1)
+        # Mise à jour de la séquence des coups joués
+        self.coup_joue += str(colonne + 1)
 
+    # Méthode pour vérifier si un coup gagnant a été joué dans une colonne
     def isWinningMove(self, colonne):
         current_player = 1 + self.moves % 2
-        if (self.height[colonne] >= 3 and self.board[colonne][self.height[colonne]-1] == current_player and
-                self.board[colonne][self.height[colonne] - 2] == current_player and self.board[colonne][self.height[colonne] - 3] == current_player):
+        # Vérification verticale
+        if (self.height[colonne] >= 3 and
+            self.board[colonne][self.height[colonne] - 1] == current_player and
+            self.board[colonne][self.height[colonne] - 2] == current_player and
+            self.board[colonne][self.height[colonne] - 3] == current_player):
             return True
 
+        # Vérification horizontale et diagonale
         for i in range(-1, 2):
             nb = 0
             for ii in [-1, 1]:
                 x = colonne + ii
-                y = self.height[colonne] + i*ii
-                while 0 <= x < self.WIDTH and 0 <= y < self.HEIGHT and self.board[x][y] == current_player:
+                y = self.height[colonne] + i * ii
+                while (0 <= x < self.WIDTH and 0 <= y < self.HEIGHT and
+                       self.board[x][y] == current_player):
                     nb += 1
                     x += ii
-                    y += i*ii
+                    y += i * ii
             if nb >= 3:
                 return True
         return False
 
+    # Méthode pour obtenir le nombre de coups joués
     def nbMove(self):
         return self.moves
 
+    # Méthode pour afficher le plateau de jeu
     def affBoard(self):
-        for j in range(5,-1,-1):
+        for j in range(5, -1, -1):
             for i in range(7):
                 print(self.board[i][j], end="|")
             print("")
 
+    # Méthode pour convertir la séquence de coups joués en plateau de jeu
     def readString(self):
-        move = len(self.coup_joue)  # Le nombre de coup joue
+        move = len(self.coup_joue)  # Le nombre de coup joué
         height = [0, 0, 0, 0, 0, 0, 0]  # Le nombre de jeton par colonne
         board = [[0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0],
@@ -51,17 +67,18 @@ class Position:
                  [0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0]]     # 0 si rien n'a été jouer dans cette emplacement
-        # 1 si le premier a avoir jouer possède le pions jouer et
+                 [0, 0, 0, 0, 0, 0]]  # 0 si rien n'a été joué dans cette emplacement
+        # 1 si le premier à avoir joué possède le pion joué et
         # 2 si c'est l'autre joueur
         for i in range(len(self.coup_joue)):
-            board[int(self.coup_joue[i])-1][height[int(self.coup_joue[i])-1]] = 1 + i % 2
+            board[int(self.coup_joue[i]) - 1][height[int(self.coup_joue[i]) - 1]] = 1 + i % 2
             height[int(self.coup_joue[i]) - 1] += 1
 
         return board, height, move
 
 
 if __name__ == "__main__":
+    # Exemple d'utilisation de la classe Position
     coup_joue = "313131"
     pos = Position(coup_joue)
     pos.affBoard()
