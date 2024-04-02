252564978beb7142ec44d8d20ac8ee3ee6d60eec
Barberey <124622426+Barberey@users.noreply.github.com>
2024-04-02 16:44:52 +0200

    Update pion.py

--- Data/pion.py
+++ Data/pion.py
@@ -1,18 +1,38 @@
-
-
 class Pion:
     def __init__(self, position, joueur):
-        self.position = position
-        self.joueur = joueur
-
+        """
+        Constructeur de la classe Pion.
+        
+        Args:
+            position (tuple): Position du pion sur le plateau (ligne, colonne).
+            joueur (Joueur): Joueur à qui appartient le pion.
+        """
+        self.position = position  # Position du pion sur le plateau
+        self.joueur = joueur  # Joueur à qui appartient le pion
 
     def get_position(self):
+        """
+        Renvoie la position actuelle du pion.
+        
+        Returns:
+            tuple: Position du pion sur le plateau (ligne, colonne).
+        """
         return self.position
 
-
     def set_position(self, position):
+        """
+        Définit la nouvelle position du pion.
+        
+        Args:
+            position (tuple): Nouvelle position du pion sur le plateau (ligne, colonne).
+        """
         self.position = position
 
-    
     def get_joueur(self):
+        """
+        Renvoie le joueur à qui appartient le pion.
+        
+        Returns:
+            Joueur: Joueur à qui appartient le pion.
+        """
         return self.joueur
