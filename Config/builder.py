1f9e47ba3f1b0d1fa0e047eee7357ab71500d013
Harlock15 <92543161+Harlock15@users.noreply.github.com>
2024-04-02 16:42:19 +0200

    Update builder.py

--- Config/builder.py
+++ Config/builder.py
@@ -1,5 +1,6 @@
 # builder.py
 
+# Importation des classes nécessaires depuis d'autres modules
 from Data.Plateau import Plateau
 from Data.Joueur import Joueur
 from Config.Configuration import Configuration
@@ -7,8 +8,23 @@ from Config.Configuration import Configuration
 class Builder:
     @staticmethod
     def init_plato():
+        """
+        Méthode statique pour initialiser un plateau de jeu.
+        
+        Returns:
+            Plateau: Instance de la classe Plateau avec le nombre de lignes et de colonnes spécifié par la configuration.
+        """
         return Plateau(Configuration.LINE_COUNT, Configuration.COLUMN_COUNT)
 
     @staticmethod
     def init_joueur(type):
+        """
+        Méthode statique pour initialiser un joueur.
+        
+        Args:
+            type (str): Le type du joueur à initialiser.
+        
+        Returns:
+            Joueur: Instance de la classe Joueur avec le type spécifié.
+        """
         return Joueur(type)
