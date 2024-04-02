48e5a759b632831b0af59fdd4cf6e00e581309be
Barberey <124622426+Barberey@users.noreply.github.com>
2024-04-02 16:41:33 +0200

    Update Case.py

--- Data/Case.py
+++ Data/Case.py
@@ -1,18 +1,48 @@
 class Case:
     def __init__(self, colonne, ligne):
+        """
+        Constructeur de la classe Case.
+        
+        Args:
+            colonne (int): La colonne de la case.
+            ligne (int): La ligne de la case.
+        """
         self.colonne = colonne
         self.ligne = ligne
-        self.pion = None
+        self.pion = None  # Initialisation de la case sans pion
 
     def get_colonne(self):
+        """
+        Renvoie la colonne de la case.
+        """
         return self.colonne
 
     def get_ligne(self):
+        """
+        Renvoie la ligne de la case.
+        """
         return self.ligne
 
     def get_pion(self):
+        """
+        Renvoie le pion actuellement dans la case.
+        """
         return self.pion
-    def set_pion(self,p):
-        self.pion=p
+
+    def set_pion(self, p):
+        """
+        Définit le pion dans la case.
+        
+        Args:
+            p (object): Le pion à placer dans la case.
+        """
+        self.pion = p
+
     def estVide(self):
+        """
+        Vérifie si la case est vide.
+        
+        Returns:
+            bool: True si la case est vide, False sinon.
+        """
         return self.pion is None
