23bf51448c6ddec81258d5575f4b4888bbc1fa86
Barberey <124622426+Barberey@users.noreply.github.com>
2024-04-02 16:42:40 +0200

    Update Joueur.py

--- Data/Joueur.py
+++ Data/Joueur.py
@@ -1,14 +1,28 @@
 class Joueur:
-
-
     def __init__(self, type):
-        self.type = type
-        self.pions = []
-
+        """
+        Constructeur de la classe Joueur.
+        
+        Args:
+            type (str): Le type de joueur (par exemple, "humain" ou "ia").
+        """
+        self.type = type  # Définition du type de joueur
+        self.pions = []   # Initialisation de la liste des pions du joueur
 
     def get_type(self):
+        """
+        Renvoie le type de joueur.
+        
+        Returns:
+            str: Le type de joueur.
+        """
         return self.type
 
-
     def get_pions(self):
+        """
+        Renvoie la liste des pions du joueur.
+        
+        Returns:
+            list: Liste des pions du joueur.
+        """
         return self.pions
