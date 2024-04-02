4d7b3f8b3605747c6e58b0694d34c14524fe0dcd
Harlock15 <92543161+Harlock15@users.noreply.github.com>
2024-04-02 16:41:58 +0200

    Update Configuration.py

--- Config/Configuration.py
+++ Config/Configuration.py
@@ -1,10 +1,21 @@
 class Configuration:
-
+    # Largeur de la fenêtre du jeu
     WINDOW_WIDTH = 700
-    WINDOW_HEIGHT = 700
-    BLOCK_SIZE = WINDOW_WIDTH // 7
-    PLATEAU_WIDTH = WINDOW_WIDTH * 7 // 5
-    PLATEAU_HEIGHT = WINDOW_HEIGHT * 7 // 5
-    LINE_COUNT = WINDOW_HEIGHT // BLOCK_SIZE
-    COLUMN_COUNT = WINDOW_WIDTH // BLOCK_SIZE
 
+    # Hauteur de la fenêtre du jeu
+    WINDOW_HEIGHT = 700
+
+    # Taille des blocs sur le plateau de jeu (basée sur la largeur de la fenêtre)
+    BLOCK_SIZE = WINDOW_WIDTH // 7
+
+    # Largeur du plateau de jeu (basée sur la largeur de la fenêtre)
+    PLATEAU_WIDTH = WINDOW_WIDTH * 7 // 5
+
+    # Hauteur du plateau de jeu (basée sur la hauteur de la fenêtre)
+    PLATEAU_HEIGHT = WINDOW_HEIGHT * 7 // 5
+
+    # Nombre de lignes sur le plateau de jeu (basé sur la hauteur de la fenêtre et la taille des blocs)
+    LINE_COUNT = WINDOW_HEIGHT // BLOCK_SIZE
+
+    # Nombre de colonnes sur le plateau de jeu (basé sur la largeur de la fenêtre et la taille des blocs)
+    COLUMN_COUNT = WINDOW_WIDTH // BLOCK_SIZE
